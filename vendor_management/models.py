3015b286a4e5cbfe078504778a83c24c650ade69
nandu44-code <nanduganeshnair4444@gmail.com>
2024-05-02 22:09:12 +0530

    Add VentorProfile model with fields. - Added VendorProfile model with fields for name, contact_details, address, vendor_code, on_time_delivery_rate, quality_rating_avg, average_response_time, and fulfillment_rate - Set vendor_code as unique identifier

--- vendor_management/models.py
+++ vendor_management/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class VendorProfile(models.Model):
+    name = models.TextField(max_length=35)
+    contact_details = models.TextField(max_length=100)
+    address = models.TextField(max_length=100)
+    vendor_code = models.CharField(max_length=20,unique=True)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fullfillment_rate = models.FloatField()
+    
