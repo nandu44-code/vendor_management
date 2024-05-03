7c06339a7056fb2c5cb358e65de0a21af20c2deb
nandu44-code <nanduganeshnair4444@gmail.com>
2024-05-04 00:11:24 +0530

    Created methods to calculate performance metrics

--- vendor_management/models.py
+++ vendor_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Avg,Sum
 import json
 
 # Create your models here.
@@ -20,6 +21,7 @@ class PurchaseOrder(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
     delivery_date = models.DateTimeField()
+    delivered_date = models.DateTimeField(null=True, blank=True)
     items = models.JSONField()
     quantity = models.IntegerField()
     status = models.CharField(max_length=20)
@@ -40,3 +42,41 @@ class HistoricalPerformance(models.Model):
 
     def __str__(self):
         return f"{self.vendor} - {self.date}"
+
+
+class VendorPerformanceService:
+    @staticmethod
+    def update_on_time_delivery_rate(vendor):
+        completed_orders = PurchaseOrder.objects.filter(vendor=vendor, status='completed')
+        if completed_orders:
+            on_time_deliveries = completed_orders.objects.filter(delivery_date__gte = F('delivered_date'))
+            on_time_delivery_rate = on_time_deliveries.count()/completed_orders.count()
+            vendor.on_time_delivery_rate = on_time_delivery_rate if on_time_delivery_rate else 0
+            vendor.save()
+
+    @staticmethod
+    def update_quality_rating_avg(vendor):
+        completed_orders_with_rating = PurchaseOrder.objects.filter(vendor=vendor, status='completed', quality_rating__isnull = False)
+        quality_rating_avg =  completed_orders_with_rating.aggregate(Avg('quality_rating'))
+        vendor.quliaty_rating_avg = quality_rating_avg
+        vendor.save()
+
+    @staticmethod
+    def update_response_time_avg(vendor):
+        response_times = PurchaseOrder.objects.filter(vendor=vendor, acknowledgment_date__isnull = False).values_list('acknowledgement_date', 'issue_date')
+        if response_times:
+            average_response_time = sum((acknowledgment_date - issue_date).total_secconds for acknowledgment_date , issue_date in response_times)/len(response_times)
+        else:
+            average_response_time = 0
+        vendor.average_response_time = average_response_time
+        vender.save()
+
+    @staticmethod
+    def update_fulfillment_rate(vendor):
+
+        fulfilled_orders = PurchaseOrder.objects.filter(vendor=vendor , status='completed')
+        total_orders = PurchaseOrder.objects.filter(vendor=vendor)
+        fulfillment_rate = fulfilled_orders.count/total_orders.count
+        ventor.fulfillment_rate = fulfillment_rate
+
+
